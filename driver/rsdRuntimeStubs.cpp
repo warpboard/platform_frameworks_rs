3e166c487db6af56258cb12d06d643bbb99db11e
Stephen Hines <srhines@google.com>
2013-12-05 17:57:49 -0800

    Add tests for rsDebug() in support library and fix exported functions.
    Functions using long vector types for rsDebug were being exported with the
    wrong signature from libRSSupport.so. Essentially it comes down to the name
    mangling rules being different for "long" and "long long" when combined with
    the ext_vector_type attribute. We now hard-code the "long" vector types in
    our type signatures and then have them be cast properly to the 64-bit types
    we were expecting.
    
    Change-Id: I8f3dad73d0f17f22f66703aff56d6f712fa6df1f

--- driver/rsdRuntimeStubs.cpp
+++ driver/rsdRuntimeStubs.cpp
@@ -1474,6 +1474,7 @@ static void SC_debugUL3(const char *s, ulong3 ll) {
 static void SC_debugUL4(const char *s, ulong4 ll) {
     ALOGD("%s {%llu, %llu, %llu, %llu}  0x%llx 0x%llx 0x%llx 0x%llx", s, ll.x, ll.y, ll.z, ll.w, ll.x, ll.y, ll.z, ll.w);
 }
+
 static void SC_debugP(const char *s, const void *p) {
     ALOGD("%s %p", s, p);
 }
@@ -1658,6 +1659,41 @@ void rsDebug(const char *s, const ulong3 *c) {
 
 void rsDebug(const char *s, const ulong4 *c) {
     SC_debugUL4(s, *c);
+}
+
+// FIXME: We need to export these function signatures for the compatibility
+// library. The C++ name mangling that LLVM uses for ext_vector_type requires
+// different versions for "long" vs. "long long". Note that the called
+// functions are still using the appropriate 64-bit sizes.
+typedef long l2 __attribute__((ext_vector_type(2)));
+typedef long l3 __attribute__((ext_vector_type(3)));
+typedef long l4 __attribute__((ext_vector_type(4)));
+typedef unsigned long ul2 __attribute__((ext_vector_type(2)));
+typedef unsigned long ul3 __attribute__((ext_vector_type(3)));
+typedef unsigned long ul4 __attribute__((ext_vector_type(4)));
+
+void rsDebug(const char *s, const l2 *c) {
+    SC_debugL2(s, *(const long2 *)c);
+}
+
+void rsDebug(const char *s, const l3 *c) {
+    SC_debugL3(s, *(const long3 *)c);
+}
+
+void rsDebug(const char *s, const l4 *c) {
+    SC_debugL4(s, *(const long4 *)c);
+}
+
+void rsDebug(const char *s, const ul2 *c) {
+    SC_debugUL2(s, *(const ulong2 *)c);
+}
+
+void rsDebug(const char *s, const ul3 *c) {
+    SC_debugUL3(s, *(const ulong3 *)c);
+}
+
+void rsDebug(const char *s, const ul4 *c) {
+    SC_debugUL4(s, *(const ulong4 *)c);
 }
 
 void rsDebug(const char *s, const void *p) {
